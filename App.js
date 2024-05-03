2848b334518c9afa5dfda72776bf8919abacac22
Gustavocorrea0 <gustavocorreaofc@gmail.com>
2024-05-03 16:51:34 -0300

    criacao da tela de inicio em App.js | 12:45 | 02/05/2024

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
+
+import styles from './style'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+      <View style={styles.containerTopo}>
+        <Text style={styles.tituloTherapyo}>Therapyo</Text>
+      </View>
+
+      <Text style={styles.tituloPagina}>Início</Text>
+
+      <View style={styles.containerBranco}>
+
+        <Text style={styles.tituloContainerBranco}>Atividades</Text>
+
+        <View style={styles.botaoUm}></View>
+
+        <View style={styles.botaoDois}></View>
+
+        <View style={styles.botaoTres}></View>
+
+        <View style={styles.botaoQuatro}></View>
+
+        <View style={styles.botaoCinco}></View>
+
+      </View>
+
+
+    </SafeAreaView>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
