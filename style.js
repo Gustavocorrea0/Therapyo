f3a04f1f99eb94a42201d49552f92b9017fd367f
Gustavocorrea0 <gustavocorreaofc@gmail.com>
2024-05-05 19:02:24 -0300

    criacao da tela de inicio | 19:00 | 05/05/2024

---
+++ style.js
@@ -0,0 +1,94 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#126EA1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  containerBranco: {
+    width: '100%',
+    height: '85%',
+    backgroundColor: '#FFFFFF',
+    marginTop: 20,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    alignItems: 'center'
+  },
+
+  containerTopo: {
+    width: '100%',
+    height: 100,
+    backgroundColor: '#09B5EB',
+    alignItems: 'center'
+  },
+
+  tituloTherapyo: {
+    fontSize: 27,
+    color: "#FFFFFF",
+    marginTop: 50,
+    fontWeight: 'bold'
+  },
+
+  tituloPagina: {
+    fontSize: 35,
+    color: "#FFFFFF",
+    marginTop: 15,
+    fontWeight: 'bold'
+  },
+
+  tituloContainerBranco: {
+    fontSize: 25,
+    color: '#000000',
+    fontWeight: 'bold',
+    marginTop: 20
+  },
+
+  botaoUm: {
+    width: 175,
+    height: 175,
+    backgroundColor: '#8996C7',
+    marginTop: 30,
+    marginLeft: -220,
+    borderRadius: 10
+  },
+
+  botaoDois: {
+    width: 175,
+    height: 175,
+    backgroundColor: '#77D6E3',
+    marginTop: 25,
+    marginLeft: -220,
+    borderRadius: 10
+  },
+
+  botaoTres: {
+    width: 175,
+    height: 175,
+    backgroundColor: '#D0CB51',
+    marginTop: -375,
+    marginLeft: 220,
+    borderRadius: 10
+  },
+
+  botaoQuatro: {
+    width: 175,
+    height: 175,
+    backgroundColor: '#96D863',
+    marginTop: 27,
+    marginLeft: 220,
+    borderRadius: 10
+  },
+
+  botaoCinco: {
+    width: 175,
+    height: 175,
+    backgroundColor: '#EDC868',
+    marginTop: 20,
+    marginLeft: 0,
+    borderRadius: 10
+  }
+
+})
